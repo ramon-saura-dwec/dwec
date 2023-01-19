cc2f36d4fddb444143554a16b86f58cfe4418f65
ramon-saura-dwec <monscave@gmail.com>
2023-01-19 19:29:29 +0100

    add some components

--- React/mi-app/src/Home.js
+++ React/mi-app/src/Home.js
@@ -9,16 +9,14 @@ function Home() {
         {title:'Fumar es un placer', body: 'bla bla bla bla', author: 'susy', id: 3}
     ]);
 
-    const [blogs2, setBlogs2] = useState([
-        {title:'Nam en pel', body: 'bla bla bla bla', author: 'my self', id: 1},
-        {title:'Fot vesa tiiiiu', body: 'bla bla bla bla', author: 'xec', id: 2},
-        {title:'Fumar es un placer', body: 'bla bla bla bla', author: 'susy', id: 3}
-    ]);
+    let blogId = [blogs.find(blog => blog.author === 'xec')];
+
+    console.log(blogId);
 
     return(
         <div className="home">
            <BlogList blogs={blogs}/>
-           <BlogList blogs={blogs2}/>
+           <BlogList blogs={blogId}/>
         </div>
     )
 }
